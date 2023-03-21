0495dd96925b505c117700f063dd6c59c241a9b0
footballjones <footballjones308@gmail.com>
2023-03-21 01:38:31 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const element= document.getElementById("main");
+element.remove("main");
+const newHeader=document.createElement(`h1`);
+newHeader.id="victory"
+newHeader.innerHTML="David Jones is the champion";
